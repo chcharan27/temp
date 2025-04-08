06895082d38a4d512b5adfa3fab1f45427d14e3f
chcharan27 <137179952+chcharan27@users.noreply.github.com>
2025-04-09 01:32:03 +0530

    Update UserDto.cs

--- WellBeing.Services.UserHealthAPI/Models/Dto/UserDto.cs
+++ WellBeing.Services.UserHealthAPI/Models/Dto/UserDto.cs
@@ -9,6 +9,23 @@
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
+        
+        public string Gender { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string EmergencyContactName { get; set; }
+
+        public string EmergencyContactNumber { get; set; }
+
+        public DateTime DateTime { get; set; }
+
 
     }
 }
