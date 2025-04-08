d286d708eb3757c4721b20a536944d80b985a6e5
Vamshi <charanchidella@gmail.com>
2025-04-09 01:30:04 +0530

    Added Dtos

---
+++ WellBeing.Services.UserHealthAPI/Models/Dto/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WellBeing.Services.UserHealthAPI.Models.Dto
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+}
